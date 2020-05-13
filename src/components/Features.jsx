76fe590fe18291c42a57fa2f6340769b3146cc20
Loquen Jones <lrjones416@gmail.com>
2020-05-13 12:06:46 -0700

    feature section with style

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
 
+const featureItems = [
+  {
+    title: 'Lorem',
+    img: '/home-expertise.svg'
+  },
+  {
+    title: 'Lorem Ipsum',
+    img: '/home-hygiene.svg'
+  },
+  {
+    title: 'Lorem Ipsum',
+    img: '/home-lab.svg'
+  },
+  {
+    title: 'Lorem',
+    img: '/home-retention.svg'
+  },
+]
+
 function Features(props) {
   return (
     <div className='features'>
-      features
+      {featureItems.map(item => 
+        <div key={item.title} className="feature-item">
+          <div className="feature-img">
+            <img src={item.img} alt={item.title}/>
+          </div>
+          <div className="feature-title">
+            {item.title}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
